f0b5c4b7865e5734017090be24592985b0ecd86b
KungFuTurtle <m.rfischer5235@gmail.com>
2024-04-10 06:51:24 -0500

    added pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is a simple homepage for your React application.</p>
+      <Link to="/search">Go to Search Page</Link>
+    </div>
+  );
+};
+
+export default HomePage;
